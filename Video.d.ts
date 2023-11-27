cce9ee410799b22e08a4b1c29a994401af68daf8
Daníque de Jong <danique.de.jong2000@gmail.com>
2023-11-27 10:05:01 +0100

    Merge branch 'react-native-video-master' beta.0.2

--- Video.d.ts
+++ Video.d.ts
@@ -84,19 +84,176 @@ export interface OnBufferData {
     isBuffering: boolean;
 }
 
-export interface DRMSettings {
-  type: DRMType;
-  licenseServer?: string | undefined;
-  headers?: { [key: string]: string } | undefined;
-  contentId?: string | undefined;
-  certificateUrl?: string | undefined;
-  base64Certificate?: boolean | undefined;
-  getLicense?(spcString: string): Promise<string>;
-}
+export type DRMSettings = Readonly<{
+    type?: DrmType;
+    licenseServer?: string;
+    headers?: Headers;
+    contentId?: string; // ios
+    certificateUrl?: string; // ios
+    base64Certificate?: boolean; // ios default: false
+    /* eslint-disable @typescript-eslint/no-unused-vars */
+    getLicense?: (
+      licenseUrl: string,
+      contentId: string,
+      spcBase64: string,
+    ) => void; // ios
+    /* eslint-enable @typescript-eslint/no-unused-vars */
+  }>;
 
-export interface AdEventType {
-    event: "AD_BREAK_ENDED" | "AD_BREAK_READY" | "AD_BREAK_STARTED" | "AD_BUFFERING" | "AD_CAN_PLAY" | "AD_METADATA" | "AD_PERIOD_ENDED" | "AD_PERIOD_STARTED" | "AD_PROGRESS" | "ALL_ADS_COMPLETED" | "CLICK" | "COMPLETE" | "COMPLETED" | "CONTENT_PAUSE_REQUESTED" | "CONTENT_RESUME_REQUESTED" | "CUEPOINTS_CHANGED" | "DURATION_CHANGE" | "FIRST_QUARTILE" | "IMPRESSION" | "INTERACTION" | "LINEAR_CHANGED" | "LOADED" | "LOG" | "MIDPOINT" | "PAUSED" | "RESUMED" | "SKIPPABLE_STATE_CHANGED" | "SKIPPED" | "STARTED" | "STREAM_LOADED" | "TAPPED" | "THIRD_QUARTILE" | "UNKNOWN" | "USER_CLOSE" | "VIDEO_CLICKED" | "VIDEO_ICON_CLICKED" | "VOLUME_CHANGED" | "VOLUME_MUTED",
-}
+export enum AdEvent {
+    /**
+     * iOS only: Fired the first time each ad break ends. Applications must reenable seeking when this occurs (only used for dynamic ad insertion).
+     */
+    AD_BREAK_ENDED = 'AD_BREAK_ENDED',
+    /**
+     * Fires when an ad rule or a VMAP ad break would have played if autoPlayAdBreaks is false.
+     */
+    AD_BREAK_READY = 'AD_BREAK_READY',
+    /**
+     * iOS only: Fired first time each ad break begins playback. If an ad break is watched subsequent times this will not be fired. Applications must disable seeking when this occurs (only used for dynamic ad insertion).
+     */
+    AD_BREAK_STARTED = 'AD_BREAK_STARTED',
+    /**
+     * Android only: Fires when the ad has stalled playback to buffer.
+     */
+    AD_BUFFERING = 'AD_BUFFERING',
+    /**
+     * Android only: Fires when the ad is ready to play without buffering, either at the beginning of the ad or after buffering completes.
+     */
+    AD_CAN_PLAY = 'AD_CAN_PLAY',
+    /**
+     * Android only: Fires when an ads list is loaded.
+     */
+    AD_METADATA = 'AD_METADATA',
+    /**
+     * iOS only: Fired every time the stream switches from advertising or slate to content. This will be fired even when an ad is played a second time or when seeking into an ad (only used for dynamic ad insertion).
+     */
+    AD_PERIOD_ENDED = 'AD_PERIOD_ENDED',
+    /**
+     * iOS only: Fired every time the stream switches from content to advertising or slate. This will be fired even when an ad is played a second time or when seeking into an ad (only used for dynamic ad insertion).
+     */
+    AD_PERIOD_STARTED = 'AD_PERIOD_STARTED',
+    /**
+     * Android only: Fires when the ad's current time value changes. Calling getAdData() on this event will return an AdProgressData object.
+     */
+    AD_PROGRESS = 'AD_PROGRESS',
+    /**
+     * Fires when the ads manager is done playing all the valid ads in the ads response, or when the response doesn't return any valid ads.
+     */
+    ALL_ADS_COMPLETED = 'ALL_ADS_COMPLETED',
+    /**
+     * Fires when the ad is clicked.
+     */
+    CLICK = 'CLICK',
+    /**
+     * Fires when the ad completes playing.
+     */
+    COMPLETED = 'COMPLETED',
+    /**
+     * Android only: Fires when content should be paused. This usually happens right before an ad is about to cover the content.
+     */
+    CONTENT_PAUSE_REQUESTED = 'CONTENT_PAUSE_REQUESTED',
+    /**
+     * Android only: Fires when content should be resumed. This usually happens when an ad finishes or collapses.
+     */
+    CONTENT_RESUME_REQUESTED = 'CONTENT_RESUME_REQUESTED',
+    /**
+     * iOS only: Cuepoints changed for VOD stream (only used for dynamic ad insertion).
+     */
+    CUEPOINTS_CHANGED = 'CUEPOINTS_CHANGED',
+    /**
+     * Android only: Fires when the ad's duration changes.
+     */
+    DURATION_CHANGE = 'DURATION_CHANGE',
+    /**
+     * Fires when the ad playhead crosses first quartile.
+     */
+    FIRST_QUARTILE = 'FIRST_QUARTILE',
+    /**
+     * Android only: Fires when the impression URL has been pinged.
+     */
+    IMPRESSION = 'IMPRESSION',
+    /**
+     * Android only: Fires when an ad triggers the interaction callback. Ad interactions contain an interaction ID string in the ad data.
+     */
+    INTERACTION = 'INTERACTION',
+    /**
+     * Android only: Fires when the displayed ad changes from linear to nonlinear, or the reverse.
+     */
+    LINEAR_CHANGED = 'LINEAR_CHANGED',
+    /**
+     * Fires when ad data is available.
+     */
+    LOADED = 'LOADED',
+    /**
+     * Fires when a non-fatal error is encountered. The user need not take any action since the SDK will continue with the same or next ad playback depending on the error situation.
+     */
+    LOG = 'LOG',
+    /**
+     * Fires when the ad playhead crosses midpoint.
+     */
+    MIDPOINT = 'MIDPOINT',
+    /**
+     * Fires when the ad is paused.
+     */
+    PAUSED = 'PAUSED',
+    /**
+     * Fires when the ad is resumed.
+     */
+    RESUMED = 'RESUMED',
+    /**
+     * Android only: Fires when the displayed ads skippable state is changed.
+     */
+    SKIPPABLE_STATE_CHANGED = 'SKIPPABLE_STATE_CHANGED',
+    /**
+     * Fires when the ad is skipped by the user.
+     */
+    SKIPPED = 'SKIPPED',
+    /**
+     * Fires when the ad starts playing.
+     */
+    STARTED = 'STARTED',
+    /**
+     * iOS only: Stream request has loaded (only used for dynamic ad insertion).
+     */
+    STREAM_LOADED = 'STREAM_LOADED',
+    /**
+     * iOS only: Fires when the ad is tapped.
+     */
+    TAPPED = 'TAPPED',
+    /**
+     * Fires when the ad playhead crosses third quartile.
+     */
+    THIRD_QUARTILE = 'THIRD_QUARTILE',
+    /**
+     * iOS only: An unknown event has fired
+     */
+    UNKNOWN = 'UNKNOWN',
+    /**
+     * Android only: Fires when the ad is closed by the user.
+     */
+    USER_CLOSE = 'USER_CLOSE',
+    /**
+     * Android only: Fires when the non-clickthrough portion of a video ad is clicked.
+     */
+    VIDEO_CLICKED = 'VIDEO_CLICKED',
+    /**
+     * Android only: Fires when a user clicks a video icon.
+     */
+    VIDEO_ICON_CLICKED = 'VIDEO_ICON_CLICKED',
+    /**
+     * Android only: Fires when the ad volume has changed.
+     */
+    VOLUME_CHANGED = 'VOLUME_CHANGED',
+    /**
+     * Android only: Fires when the ad volume has been muted.
+     */
+    VOLUME_MUTED = 'VOLUME_MUTED',
+};  
+
+export type OnReceiveAdEventData = Readonly<{
+    event: AdEvent;
+}>;
 
 export const TextTrackType: {
     SRT: 'application/x-subrip';
@@ -130,6 +287,26 @@ export enum DRMType {
   FAIRPLAY = 'fairplay',
 }
 
+export type Headers = Record<string, string>;
+
+export type VideoSrc = Readonly<{
+    uri?: string;
+    isNetwork?: boolean;
+    isAsset?: boolean;
+    shouldCache?: boolean;
+    type?: string;
+    mainVer?: number;
+    patchVer?: number;
+    requestHeaders?: Headers;
+    startPosition?: number;
+    cropStart?: number;
+    cropEnd?: number;
+    title?: string;
+    subtitle?: string;
+    description?: string;
+    customImageUri?: string;
+  }>;  
+
 export interface VideoProperties extends ViewProps {
     filter?: FilterType | undefined;
     filterEnabled?: boolean | undefined;
@@ -156,7 +333,7 @@ export interface VideoProperties extends ViewProps {
 
     /* Wrapper component */
     // Opaque type returned by require('./video.mp4')
-    source: { uri?: string | undefined, headers?: {[key: string]: string } | undefined, type?: string | undefined } | number;
+    source: VideoSrc;
     minLoadRetryCount?: number | undefined;
     maxBitRate?: number | undefined;
     resizeMode?: "stretch" | "contain" | "cover" | "none" | undefined; // via Image#resizeMode
@@ -241,7 +418,7 @@ export interface VideoProperties extends ViewProps {
     translateY?: number | undefined;
     rotation?: number | undefined;
     adTagUrl?: string | undefined;
-    onReceiveAdEvent?(data: AdEventType): void;
+    onReceiveAdEvent?: (e: OnReceiveAdEventData) => void; //Android, iOS
 }
 
 export default class Video extends React.Component<VideoProperties> {
--- android/src/main/java/com/brentvatne/exoplayer/ReactExoplayerView.java
+++ android/src/main/java/com/brentvatne/exoplayer/ReactExoplayerView.java
@@ -61,6 +61,7 @@ import androidx.media3.exoplayer.drm.FrameworkMediaDrm;
 import androidx.media3.exoplayer.drm.HttpMediaDrmCallback;
 import androidx.media3.exoplayer.drm.UnsupportedDrmException;
 import androidx.media3.exoplayer.hls.HlsMediaSource;
+import androidx.media3.exoplayer.ima.ImaAdsLoader;
 import androidx.media3.exoplayer.mediacodec.MediaCodecInfo;
 import androidx.media3.exoplayer.mediacodec.MediaCodecUtil;
 import androidx.media3.exoplayer.smoothstreaming.DefaultSsChunkSource;
@@ -194,8 +195,9 @@ public class ReactExoplayerView extends FrameLayout implements
     // Props from React
     private int backBufferDurationMs = DefaultLoadControl.DEFAULT_BACK_BUFFER_DURATION_MS;
     private Uri srcUri;
-    private long startTimeMs = -1;
-    private long endTimeMs = -1;
+    private long startPositionMs = -1;
+    private long cropStartMs = -1;
+    private long cropEndMs = -1;
     private String extension;
     private boolean repeat;
     private String audioTrackType;
@@ -351,6 +353,7 @@ public class ReactExoplayerView extends FrameLayout implements
     @Override
     public void onHostDestroy() {
         stopPlayback();
+        themedReactContext.removeLifecycleEventListener(this);
     }
 
     public void cleanUpResources() {
@@ -617,10 +620,14 @@ public class ReactExoplayerView extends FrameLayout implements
                         .setExtensionRendererMode(DefaultRenderersFactory.EXTENSION_RENDERER_MODE_OFF);
 
         // Create an AdsLoader.
-        adsLoader = new ImaAdsLoader.Builder(themedReactContext).setAdEventListener(this).build();
+        adsLoader = new ImaAdsLoader.Builder(themedReactContext)
+                .setAdEventListener(this)
+                .build();
 
-        MediaSource.Factory mediaSourceFactory = new DefaultMediaSourceFactory(mediaDataSourceFactory)
-                .setLocalAdInsertionComponents(unusedAdTagUri -> adsLoader, exoPlayerView);
+        DefaultMediaSourceFactory mediaSourceFactory = new DefaultMediaSourceFactory(mediaDataSourceFactory);
+        if (adsLoader != null) {
+            mediaSourceFactory.setLocalAdInsertionComponents(unusedAdTagUri -> adsLoader, exoPlayerView);
+        }
 
         player = new ExoPlayer.Builder(getContext(), renderersFactory)
                 .setTrackSelector(self.trackSelector)
@@ -663,10 +670,10 @@ public class ReactExoplayerView extends FrameLayout implements
 
     private void initializePlayerSource(ReactExoplayerView self, DrmSessionManager drmSessionManager) {
         ArrayList<MediaSource> mediaSourceList = buildTextSources();
-        MediaSource videoSource = buildMediaSource(self.srcUri, self.extension, drmSessionManager, startTimeMs, endTimeMs);
+        MediaSource videoSource = buildMediaSource(self.srcUri, self.extension, drmSessionManager, cropStartMs, cropEndMs);
         MediaSource mediaSourceWithAds = null;
-        if (adTagUrl != null) {
-            MediaSource.Factory mediaSourceFactory = new DefaultMediaSourceFactory(mediaDataSourceFactory)
+        if (adTagUrl != null && adsLoader != null) {
+            DefaultMediaSourceFactory mediaSourceFactory = new DefaultMediaSourceFactory(mediaDataSourceFactory)
                     .setLocalAdInsertionComponents(unusedAdTagUri -> adsLoader, exoPlayerView);
             DataSpec adTagDataSpec = new DataSpec(adTagUrl);
             mediaSourceWithAds = new AdsMediaSource(videoSource, adTagDataSpec, ImmutableList.of(srcUri, adTagUrl), mediaSourceFactory, adsLoader, exoPlayerView);
@@ -704,7 +711,12 @@ public class ReactExoplayerView extends FrameLayout implements
         if (haveResumePosition) {
             player.seekTo(resumeWindow, resumePosition);
         }
-        player.prepare(mediaSource, !haveResumePosition, false);
+        if (startPositionMs >= 0) {
+            player.setMediaSource(mediaSource, startPositionMs);
+        } else {
+            player.setMediaSource(mediaSource, !haveResumePosition);
+        }
+        player.prepare();
         playerNeedsSource = false;
 
         reLayout(exoPlayerView);
@@ -762,7 +774,7 @@ public class ReactExoplayerView extends FrameLayout implements
         }
     }
 
-    private MediaSource buildMediaSource(Uri uri, String overrideExtension, DrmSessionManager drmSessionManager, long startTimeMs, long endTimeMs) {
+    private MediaSource buildMediaSource(Uri uri, String overrideExtension, DrmSessionManager drmSessionManager, long cropStartMs, long cropEndMs) {
         if (uri == null) {
             throw new IllegalStateException("Invalid video uri");
         }
@@ -823,12 +835,12 @@ public class ReactExoplayerView extends FrameLayout implements
                 )
                 .createMediaSource(mediaItem);
 
-        if (startTimeMs >= 0 && endTimeMs >= 0) {
-            return new ClippingMediaSource(mediaSource, startTimeMs * 1000, endTimeMs * 1000);
-        } else if (startTimeMs >= 0) {
-            return new ClippingMediaSource(mediaSource, startTimeMs * 1000, TIME_END_OF_SOURCE);
-        } else if (endTimeMs >= 0) {
-            return new ClippingMediaSource(mediaSource, 0, endTimeMs * 1000);
+        if (cropStartMs >= 0 && cropEndMs >= 0) {
+            return new ClippingMediaSource(mediaSource, cropStartMs * 1000, cropEndMs * 1000);
+        } else if (cropStartMs >= 0) {
+            return new ClippingMediaSource(mediaSource, cropStartMs * 1000, TIME_END_OF_SOURCE);
+        } else if (cropEndMs >= 0) {
+            return new ClippingMediaSource(mediaSource, 0, cropEndMs * 1000);
         }
 
         return mediaSource;
@@ -883,7 +895,6 @@ public class ReactExoplayerView extends FrameLayout implements
         }
         adsLoader = null;
         progressHandler.removeMessages(SHOW_PROGRESS);
-        themedReactContext.removeLifecycleEventListener(this);
         audioBecomingNoisyReceiver.removeListener();
         bandwidthMeter.removeEventListener(this);
 
@@ -1537,13 +1548,14 @@ public class ReactExoplayerView extends FrameLayout implements
 
     // ReactExoplayerViewManager public api
 
-    public void setSrc(final Uri uri, final long startTimeMs, final long endTimeMs, final String extension, Map<String, String> headers) {
+    public void setSrc(final Uri uri, final long startPositionMs, final long cropStartMs, final long cropEndMs, final String extension, Map<String, String> headers) {
         if (uri != null) {
-            boolean isSourceEqual = uri.equals(srcUri) && startTimeMs == this.startTimeMs && endTimeMs == this.endTimeMs;
+            boolean isSourceEqual = uri.equals(srcUri) && cropStartMs == this.cropStartMs && cropEndMs == this.cropEndMs;
             hasDrmFailed = false;
             this.srcUri = uri;
-            this.startTimeMs = startTimeMs;
-            this.endTimeMs = endTimeMs;
+            this.startPositionMs = startPositionMs;
+            this.cropStartMs = cropStartMs;
+            this.cropEndMs = cropEndMs;
             this.extension = extension;
             this.requestHeaders = headers;
             this.mediaDataSourceFactory =
@@ -1561,8 +1573,9 @@ public class ReactExoplayerView extends FrameLayout implements
             player.stop();
             player.clearMediaItems();
             this.srcUri = null;
-            this.startTimeMs = -1;
-            this.endTimeMs = -1;
+            this.startPositionMs = -1;
+            this.cropStartMs = -1;
+            this.cropEndMs = -1;
             this.extension = null;
             this.requestHeaders = null;
             this.mediaDataSourceFactory = null;
